4352b17c547b60e4d6df6afae739c8ab00c79986
Dima995-gdm <1gold003>
2021-03-31 21:10:06 +0500

    add model Application

--- stepik_vacancies/urls.py
+++ stepik_vacancies/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from vacancies import views
 
 urlpatterns = [
@@ -29,3 +32,7 @@ urlpatterns = [
 
 handler404 = views.custom_handler404
 handler500 = views.custom_handler500
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- vacancies/models.py
+++ vacancies/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from stepik_vacancies.settings import MEDIA_COMPANY_IMAGE_DIR, MEDIA_SPECIALITY_IMAGE_DIR
 
 
 class Vacancy(models.Model):
@@ -18,9 +21,10 @@ class Vacancy(models.Model):
 class Company(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
-    logo = models.URLField(default='https://place-hold.it/100x60')
+    logo = models.ImageField(upload_to=MEDIA_COMPANY_IMAGE_DIR)
     description = models.CharField(max_length=100)
     employee_count = models.IntegerField()
+    owner = models.OneToOneField(User, related_name='owner_of_company', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return f'{self.pk} {self.name}'
@@ -29,7 +33,27 @@ class Company(models.Model):
 class Specialty(models.Model):
     code = models.CharField(max_length=40)
     title = models.CharField(max_length=50)
-    picture = models.URLField(default='https://place-hold.it/100x60')
+    picture = models.ImageField(upload_to=MEDIA_SPECIALITY_IMAGE_DIR)
 
     def __str__(self):
         return f'{self.pk} {self.title}'
+
+
+class Application(models.Model):
+    written_username = models.CharField(max_length=100)
+    written_phone = models.CharField(max_length=20)
+    written_cover_letter = models.TextField()
+    vacancy = models.ForeignKey(Vacancy, related_name='applications', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='applications', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.pk} {self.written_username}'
+
+
+
+
+
+
+
+
+
