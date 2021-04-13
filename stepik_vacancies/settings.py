0741df4b871e67a1b09fa4affd7eb750e92dd850
Dima995-gdm <1gold003>
2021-04-13 22:04:13 +0500

    completed

--- stepik_vacancies/settings.py
+++ stepik_vacancies/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'phonenumber_field',
     'crispy_forms',
     'accounts',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -119,11 +120,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "mysite/static"),
 ]
+
+AWS_ACCESS_KEY_ID = 'AKIAIT2Z5TDYPX3ARJBA'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'sibtc-assets'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'  # <-- here is where we reference it
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
